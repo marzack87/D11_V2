9205a5cbec38ce03f4a9c99a5dab7a5b1ccf2238
Marco Zaccheroni <marco.zaccheroni2@studio.unibo.it>
2015-04-15 15:32:26 +0200

    Yo!

---
+++ D11_V2/D11_V2.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _D11_V2_H_
+#define _D11_V2_H_
+#include "Arduino.h"
+//add your includes for the project D11_V2 here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project D11_V2 here
+
+
+
+
+//Do not add code below this line
+#endif /* _D11_V2_H_ */
